2eac4b798f8c7abd4d28e3e4a9bdd125f2811faa
Joan Jané Ballester <jjane@plainconcepts.com>
2023-05-04 12:55:01 +0200

    Add SQL Server backplane mode as a demonstration

--- README.md
+++ README.md
@@ -1,24 +1,34 @@
 # SignalR scale-out example
 
-Example for scaling out signalr (asp.net 6) on multiple front-end servers using Redis as backplane. 
+Example for scaling out signalr (asp.net 6) on multiple front-end servers using Redis/SQL Server as backplane. 
 This is a fork of [bremnes](https://github.com/bremnes/signalr-scale-out) repository, which simplifies setup and updates all dependencies.
 
 ![](https://docs.microsoft.com/en-us/aspnet/signalr/overview/performance/scaleout-in-signalr/_static/image2.png)
 
 [source](https://docs.microsoft.com/en-us/aspnet/signalr/overview/performance/scaleout-in-signalr)
 
+## Configuration
+To configure the SignalR backplane, choose between redis, sqlserver or empty to not use any backplane on appsettings.json.
+Override SignalR__Backplane default value on docker-compose.yml to change behavior on the containers.
+
+```
+  "SignalR": {
+    "Backplane": "redis" // Values: "redis" || "sqlserver" || ""
+  }
+```
+
 ## Usage:
 
 ### Docker
 1. Open a command line into src folder.
 2. Build docker images `docker compose build web`
-3. Launch Redis and 2 web instances: `docker compose up`
+3. Launch Redis/SqlServer and 2 web instances: `docker compose up`
 4. Test Web 1 on http://localhost:3010
 5. Test Web 2 on http://localhost:3011
 
 ### .NET CLI
 1. Open a command line into src folder.
-2. Launch redis with docker: `docker compose up redis`
+2. Launch redis with docker: `docker compose up redis sqlserver`
 3. Launch one instance of Web with `dotnet run --launch-profile Web`
 4. Launch a second instance of Web with `dotnet run --launch-profile Web2`
 5. Test Web 1 on http://localhost:3010
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,15 +1,48 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Web;
+
+const string REDIS_BACKPLANE = "redis";
+const string SQLSERVER_BACKPLANE = "sqlserver";
 
 const string SIGNALR_CORS_POLICY = "SIGNALR";
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 
-services.AddSignalR()
-    .AddStackExchangeRedis(builder.Configuration["Redis:ConnectionString"])
-    ;
+var backplane = builder.Configuration.GetValue("SignalR:Backplane", REDIS_BACKPLANE);
+var signalRBuilder = services.AddSignalR();
+if (backplane == REDIS_BACKPLANE)
+{
+    signalRBuilder.AddStackExchangeRedis(builder.Configuration["Redis:ConnectionString"]);
+} 
+else if (backplane == SQLSERVER_BACKPLANE)
+{
+    signalRBuilder.AddSqlServer(o =>
+    {
+        o.ConnectionString = builder.Configuration["Sql:ConnectionString"];
+        // See above - attempts to enable Service Broker on the database at startup
+        // if not already enabled. Default false, as this can hang if the database has other sessions.
+        o.AutoEnableServiceBroker = true;
+        // Every hub has its own message table(s). 
+        // This determines the part of the table named that is derived from the hub name.
+        // IF THIS IS NOT UNIQUE AMONG ALL HUBS, YOUR HUBS WILL COLLIDE AND MESSAGES MIX.
+        o.TableSlugGenerator = hubType => hubType.Name;
+        // The number of tables per Hub to use. Adding a few extra could increase throughput
+        // by reducing table contention, but all servers must agree on the number of tables used.
+        // If you find that you need to increase this, it is probably a hint that you need to switch to Redis.
+        o.TableCount = 1;
+        // The SQL Server schema to use for the backing tables for this backplane.
+        o.SchemaName = "SignalRCore";
+    });
+} 
+else
+{
+    backplane = string.Empty;
+}
+
 services.AddControllers();
 services.AddRazorPages()
     .AddRazorRuntimeCompilation();
@@ -21,11 +54,13 @@ services.AddCors(options =>
             .AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader()
-            //.AllowCredentials()
             );
 });
 
 var app = builder.Build();
+var logger = app.Services.GetService<ILogger<Program>>();
+logger.LogInformation($"Using SignalR backplane '{backplane}'");
+
 app.UseDeveloperExceptionPage();
 
 app.UseCors(SIGNALR_CORS_POLICY);
